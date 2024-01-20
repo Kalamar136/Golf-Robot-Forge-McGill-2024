58db32d58ab6e1a2a267bf363ccb4911a5b68391
Kalamar136 <karl.bridi@mail.mcgill.ca>
2024-01-20 12:51:30 -0500

    Merging Jules' code

--- forgeMcGill/forgeMcGill.ino
+++ forgeMcGill/forgeMcGill.ino
@@ -1,28 +1,41 @@
+#include <ezButton.h>
 #include <Servo.h>
 
-//Right motor
-#define A_1ARight 2
-#define A_1BRight 3
-//Left motor
-#define A_1ALeft 4
-#define A_1BLeft 5
+#define L_1A 3
+#define L_1B 2
+#define R_1A 4
+#define R_1B 5
 
 #define ClubPin = 3;
 int stop = 90;
 Servo Club;
+int motorpin = 8;
+
+//Joystick
+#define VRX_PIN A4 // Arduino pin connected to VRX pin
+#define VRY_PIN A5 // Arduino pin connected to VRY pin
+#define SW_PIN 8  // Arduino pin connected to SW  pin
+ezButton button(SW_PIN);
+
+int x = 0;
+int y = 0;
+int b = 0;
+bool wasPressed = false;
 
 void setup() {
   //Right motor setup
-  pinMode(A_1ARight, OUTPUT);
-  pinMode(A_1ARight, OUTPUT);
-  //Left motor setup
-  pinMode(A_1ALeft, OUTPUT);
-  pinMode(A_1ALeft, OUTPUT);
+  pinMode(L_1A, OUTPUT);
+  pinMode(L_1B, OUTPUT);
+  pinMode(R_1A, OUTPUT);
+  pinMode(R_1B, OUTPUT);
   
   //Club setup
-  Club.attach(motorPin);
+  Club.attach(9);
   Club.write(90);
 
+  Serial.begin(9600);
+  
+  button.setDebounceTime(50); // set debounce time to 50 milliseconds
 }
 
 //Function signatures
@@ -31,37 +44,110 @@ void backward(int A_1A, int A_1B);
 void brake(int A_1A, int A_1B);
 
 void loop() {
-  forward();
-  brake();
-  backward();
-  brake();
 
-  myservo.write(45);
-  delay(2000);
+  //myservo.write(45);
+  //delay(2000);
 
-  myservo.write(135);
-  delay(2000);
+  //myservo.write(135);
+  //delay(2000);
 
-  myservo.write(90);
-  delay(2000);
+  //myservo.write(90);
+  //delay(2000);
+
+  // put your main code here, to run repeatedly:
+  button.loop(); // MUST call the loop() function first
+  x = analogRead(VRX_PIN);
+  y = analogRead(VRY_PIN);
+  // Read the button value
+  b = button.getState();
+
+  if (button.isPressed()) {
+    Serial.println("The button is pressed");
+    // TODO do something here
+  }
+
+  if (button.isReleased()) {
+    Serial.println("The button is released");
+    // TODO do something here
+    wasPressed = !wasPressed;
+  }
+
+  if (!wasPressed) {
+    brake();
+  } else {
+    if (x > 950) {
+      right();
+    } else if (x < 74) {
+      left();
+    } else if (y > 950) {
+      forward();
+    } else if (y < 74) {
+      backward();
+    } else {
+      brake();
+    }
+  }
+  Serial.print("x = ");
+  Serial.print(x);
+  Serial.print(", y = ");
+  Serial.println(y);
+  delay(100);
 }
 
 //Functions
 
-void forward(int A_1A, int A_1B) {
-  digitalWrite(A_1A, LOW);
-  digitalWrite(A_1B, HIGH);
-  delay(1000);
+void forward() {
+  digitalWrite(L_1A, LOW);
+  digitalWrite(L_1B, HIGH);
+  digitalWrite(R_1A, LOW);
+  digitalWrite(R_1B, HIGH);
+  delay(100);
 }
 
-void backward(int A_1A, int A_1B) {
-  digitalWrite(A_1A, HIGH);
-  digitalWrite(A_1B, LOW);
-  delay(1000);
+void right() {
+  digitalWrite(L_1A, LOW);
+  digitalWrite(L_1B, HIGH);
+  digitalWrite(R_1A, HIGH);
+  digitalWrite(R_1B, LOW);
+  delay(100);
 }
 
-void brake(int A_1A, int A_1B) {
-  digitalWrite(A_1A, LOW);
-  digitalWrite(A_1B, LOW);
-  delay(1000);
+void left() {
+  digitalWrite(L_1A, HIGH);
+  digitalWrite(L_1B, LOW);
+  digitalWrite(R_1A, LOW);
+  digitalWrite(R_1B, HIGH);
+  delay(100);
 }
+
+void backward() {
+  digitalWrite(L_1A, HIGH);
+  digitalWrite(L_1B, LOW);
+  digitalWrite(R_1A, HIGH);
+  digitalWrite(R_1B, LOW);
+  delay(100);
+}
+
+void brake() {
+  digitalWrite(L_1A, LOW);
+  digitalWrite(L_1B, LOW);
+  digitalWrite(R_1A, LOW);
+  digitalWrite(R_1B, LOW);
+  delay(100);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
